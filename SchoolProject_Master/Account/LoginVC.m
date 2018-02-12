a6badd35fefbedf1da85e9ca6582cfe78ec21ab2
vsparthee <vsparthee@gmail.com>
2018-02-12 23:39:15 +0530

    update

--- SchoolProject_Master/Account/LoginVC.m
+++ SchoolProject_Master/Account/LoginVC.m
@@ -17,8 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    _txtUserName.text = @"parent1";
-    _txtPassword.text = @"123456";
+  //  _txtUserName.text = @"parent1";
+  //  _txtPassword.text = @"123456";
     
     CAShapeLayer * maskLayer = [CAShapeLayer layer];
     maskLayer.path = [UIBezierPath bezierPathWithRoundedRect: self.changeLangbtn.bounds byRoundingCorners: UIRectCornerTopLeft | UIRectCornerBottomLeft cornerRadii: (CGSize){10.0, 10.0}].CGPath;
